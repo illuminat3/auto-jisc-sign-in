744879080a055dc51d9da66a0af8fb99d3032079
Matthew <mrawson@redev.co.uk>
2024-09-21 15:19:03 +0100

    Implemented package installer

--- Jisc-Extractor/main.py
+++ Jisc-Extractor/main.py
@@ -1,7 +1,12 @@
+from Services.Setup import *
 from Services.JiscTokenExtractor import *
 from Services.JiscEventsExtractor import *
 
 def Main() -> None:
+    setup = Setup("Jisc-Extractor/requirements.txt")
+
+    setup.install_packages
+
     jiscTokenExtractor = JiscTokenExtractor()
 
     headers = jiscTokenExtractor.GetJiscHeaders()
