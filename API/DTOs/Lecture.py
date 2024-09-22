5464eecfe7c9ff96929bd093566ca858249dc750
Matthew <mrawson@redev.co.uk>
2024-09-22 21:54:12 +0100

    Added Lecture DTO

---
+++ API/DTOs/Lecture.py
@@ -0,0 +1,5 @@
+class Lecture:
+    def __init__(self, id, currentSearchValue=0, foundCode=-1):
+        self.id = id
+        self.currentSearchValue = currentSearchValue
+        self.foundCode = foundCode
