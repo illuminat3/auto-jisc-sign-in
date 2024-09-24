0b4e62d7ffe81739d1e04be2a837981f78f6db4a
MatthewRawson <mrawson@redev.co.uk>
2024-09-24 16:35:05 +0100

    Fixed setup not working

--- Jisc-Extractor/main.py
+++ Jisc-Extractor/main.py
@@ -1,8 +1,4 @@
 from Services.Setup import *
-from Services.JiscTokenExtractor import *
-from Services.JiscEventsExtractor import *
-from Services.JiscCodeBreaker import *
-from Services.SystemProfiler import *
 
 def Main() -> None:
 
@@ -12,10 +8,7 @@ def Main() -> None:
     email = "" 
     # DO NOT COMMIT WITH ACTUAL VALUES!!!
 
-    setup = Setup("Jisc-Extractor/requirements.txt")
     systemProfiler = SystemProfiler()
-
-    setup.install_packages()
 
     jiscTokenExtractor = JiscTokenExtractor(username, password, email)
 
@@ -31,4 +24,13 @@ def Main() -> None:
     
 
 if __name__ == '__main__':
+    setup = Setup("Jisc-Extractor/requirements.txt")
+
+    setup.install_packages()
+
+    from Services.JiscTokenExtractor import *
+    from Services.JiscEventsExtractor import *
+    from Services.JiscCodeBreaker import *
+    from Services.SystemProfiler import *
+
     Main()
